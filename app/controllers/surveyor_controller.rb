670d074ad59d43a08b2b6564adb3ee7273872e7e
Dana Spisak <dana_spisak@mentor.com>
2015-04-12 22:19:14 -0500

    add authentication to surveyor

--- app/controllers/surveyor_controller.rb
+++ app/controllers/surveyor_controller.rb
@@ -4,6 +4,7 @@ module SurveyorControllerCustomMethods
     # base.send :before_filter, :require_user   # AuthLogic
     # base.send :before_filter, :login_required  # Restful Authentication
     # base.send :layout, 'surveyor_custom'
+    base.send :before_filter, :authenticate_user!
   end
 
   # Actions
